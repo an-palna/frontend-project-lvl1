5698ef421362ce9ff37c14d6b53ffd03b400d56a
an-palna <ane4ka1003@yandex.ru>
2020-05-12 19:12:54 +0300

    'first'

---
+++ src/games/Even.js
@@ -0,0 +1,13 @@
+let name = prompt("May I have your name?");
+console.log("Hello,", name, "!");
+const num = Math.floor(Math.random()*100);
+console.log(num);
+let isEven = 'yes';
+if (num % 2 !== 0) {
+	isEven = 'no';
+}
+let str = prompt("Is number " + num + " even?");
+str === isEven ? console.log('Correct!') : console.log("Sorry! It's incorrect.");
+
+
+

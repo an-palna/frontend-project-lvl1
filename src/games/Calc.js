591822fff2ac1c9d4afa9b71a323d34b9939e25b
an-palna <ane4ka1003@yandex.ru>
2020-05-13 15:11:38 +0300

    'secondgame'

--- src/games/Calc.js
+++ src/games/Calc.js
@@ -1,8 +1,23 @@
 let name = prompt("May I have your name?");
 console.log("Hello,", name, "!");
+const operation = ['+', '*', '-'];
 const num1 = Math.floor(Math.random()*100);
 const num2 = Math.floor(Math.random()*100);
-const sum = num1 + num2;
-console.log(num1 + ' + ' + num2);
-let str = prompt('What is sum?');
-str == sum ? console.log('Correct!') : console.log("Sorry! It's incorrect.");
+const thisOperation = operation[Math.floor(Math.random()*3)];
+console.log(num1 + thisOperation + num2);
+let str = prompt('What is calc?');
+let calc;
+switch(thisOperation) {
+	case '+':
+	calc = num1 + num2;
+	break;
+	
+	case '*':
+	calc = num1 * num2;
+	break;
+	
+	case '-':
+	calc = num1 - num2;
+	break;
+}
+str == calc ? console.log('Correct!') : console.log("Sorry! It's incorrect.");

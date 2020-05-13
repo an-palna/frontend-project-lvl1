ae0f0b5b957de19959e44ee9da244dd724f71fe3
an-palna <ane4ka1003@yandex.ru>
2020-05-13 13:25:45 +0300

    'newSecond'

--- src/games/Calc.js
+++ src/games/Calc.js
@@ -3,6 +3,6 @@ console.log("Hello,", name, "!");
 const num1 = Math.floor(Math.random()*100);
 const num2 = Math.floor(Math.random()*100);
 const sum = num1 + num2;
-console.log(num1,  "+" , num2);
+console.log(num1 + ' + ' + num2);
 let str = prompt('What is sum?');
 str == sum ? console.log('Correct!') : console.log("Sorry! It's incorrect.");

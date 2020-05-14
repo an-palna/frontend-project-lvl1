65123810ae72d59e7155199a27dc1aad96571ddc
an-palna <ane4ka1003@yandex.ru>
2020-05-14 19:21:50 +0300

    'newsecond'

--- src/games/Calc.js
+++ src/games/Calc.js
@@ -1,12 +1,10 @@
 import random from '../randomaizer.js';
 
 let name = prompt("May I have your name?");
-console.log("Hello,", name, "!");
 const operation = ['+', '*', '-'];
 const num1 = random(0, 100);
 const num2 = random(0, 100);
 const thisOperation = operation[random(0, operation.length)];
-let str = prompt('What is calc ' + num1 + thisOperation + num2 + ' ?');
 let calc;
 switch(thisOperation) {
 	case '+':
@@ -21,4 +19,5 @@ switch(thisOperation) {
 	calc = num1 - num2;
 	break;
 }
+let str = prompt(name + ', what is calc ' + num1 + thisOperation + num2 + ' ?');
 str == calc ? alert('Correct!') : alert("Sorry! It's incorrect.");
